5fe3905107054b3b6f781d2b693f9de6604c58f8
Harsh Dutt <harsh.d@menerva.tech>
2024-05-12 21:23:53 +0530

    Api working for verification

--- script.py
+++ script.py
@@ -5,19 +5,27 @@ from PIL import Image
 import io
 import numpy as np
 
+def base64_to_numpy(base64_string):
+    image_bytes = base64.b64decode(base64_string)
+
+    image = Image.open(io.BytesIO(image_bytes))
+
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+
+    numpy_array = np.array(image)
+
+    return numpy_array
+
 def verifyFace(faces, face):
-    face_image = np.array(Image.open(io.BytesIO(base64.b64decode(face))))
+    face_image = base64_to_numpy(face)
 
-    for img in faces:
-        decoded_data = base64.b64decode(img)
-
-        # Convert to PIL Image
-        image = Image.open(io.BytesIO(decoded_data))
+    for img in faces:    
         result = DeepFace.verify(
-            img1_path = np.array(image),
-            img2_path = face_image,
+            img1_path = face_image,  # Assuming there's only one face detected
+            img2_path = base64_to_numpy(img),  # Assuming verification against the same face
             model_name = "GhostFaceNet",
-            )
+        )
         if result["verified"]:
             return result
     return result
